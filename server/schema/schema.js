c3f4670aa078b7e5605f46a265499d34fe0ee30c
HR <hrui1990@gmail.com>
2018-10-20 13:13:36 -0500

    update root query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,14 +13,18 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
+
 //some dummy data
 var books = [
   { name: "PogChamp", genre: "Sports", id: "1", authorId: "3" },
   { name: "WutFace", genre: "Horror", id: "2", authorId: "1" },
   { name: "PagChomp", genre: "Thriller", id: "3", authorId: "2" },
-  { name: "OMEGALUL", genre: "Comedy", id: "4", authorId: "3" }
+  { name: "OMEGALUL", genre: "Comedy", id: "4", authorId: "3" },
+  { name: "DansGame", genre: "Horror", id: "5", authorId: "2" },
+  { name: "PogU", genre: "Sports", id: "6", authorId: "1" }
 ];
 
 var authors = [
@@ -32,6 +36,8 @@ var authors = [
 const BookType = new GraphQLObjectType({
   name: "Book",
   //needs to be a function because we are going to add different types
+  //if they are not functions, the top to bottom approach will create a lot of undefined fields
+  // using functions, they will be defined and being used when called
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -53,7 +59,14 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      //can't use BookType because that indicates each author only have one book
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
@@ -78,6 +91,18 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
